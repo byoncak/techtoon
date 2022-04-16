ba5790fe515380c1ae54667781f2c612c20b10b5
Brett Yoncak <byoncak@gmail.com>
2022-04-16 02:23:21 -0400

    falling asleep zzzzzz

--- capstone/vue/src/views/Home.vue
+++ capstone/vue/src/views/Home.vue
@@ -1,17 +1,18 @@
 <template>
   <div class="home">
     <home-hero/>
-    <my-comics/>
+    <comic-list/>
   </div>
 </template>
 
 <script>
+import ComicList from '../components/ComicList.vue';
 import HomeHero from '../components/HomeHero.vue';
 // import ComicList from '../components/ComicList.vue';
-import MyComics from '../components/MyComics.vue';
+// import MyComics from '../components/MyComics.vue';
 
 export default {
-  components: { HomeHero, MyComics },
+  components: { HomeHero, ComicList },
   name: "home"
 };
 </script>
