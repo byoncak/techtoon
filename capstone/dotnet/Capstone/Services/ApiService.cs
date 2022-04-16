bc87d6a209d8b246a11dd060f99fe01e279e1ab9
Adam Siry <adam.siry@gmail.com>
2022-04-16 15:08:17 -0400

    place holder

--- capstone/dotnet/Capstone/Services/ApiService.cs
+++ capstone/dotnet/Capstone/Services/ApiService.cs
@@ -212,7 +212,7 @@ namespace Capstone.Services
             }
             else
             {
-                comic.CoverImage = @"src\assets\marvelPlaceholder.png";
+                comic.CoverImage = @"https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_FMjpg_UX1000_.jpg";
             }
 
             return comic;
