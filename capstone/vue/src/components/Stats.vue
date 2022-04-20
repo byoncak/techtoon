8fbfea134feb6637e03ba3aae78f448d08e2cf13
Lee Chan <x3nahceel@gmail.com>
2022-04-20 14:21:17 -0400

    update collection stats

---
+++ capstone/vue/src/components/Stats.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="leaderboard-container">
+        <div class="columns">
+        <h4 class="leaderboard-title">Statistics:</h4>
+        <div class="leaders-container">
+        <div class="leaders" v-for="character in characters" v-bind:key="character.id">
+        <div class="leader-stats">
+            <p class="leaders" id="leader-name">{{character.name}}</p> <p class="leaders"> {{character.issueCount}}</p>
+           </div>
+        </div>
+        </div>
+        <p>Total Number of book in this Collection: {{totalComic.totalComicCount}}</p>
+        </div>
+    </div>
+
+</template>
+
+<script>
+import localService from '@/services/LocalService';
+
+export default {
+    name:"stats-board",
+    props:['collectionId'],
+    data(){
+        return{
+            characters:[],
+            totalComic:''
+        }
+    },
+    created(){
+        localService.getComicStatCharacter(this.collectionId).then(response =>{
+                this.characters=response.data;
+            });
+        localService.getComicTotalInCollection(this.collectionId).then(response =>{
+                this.totalComic=response.data;
+            });
+    },
+    
+
+
+}
+</script>
+
+<style>
+
+</style>
--- capstone/vue/src/views/PublicComicList.vue
+++ capstone/vue/src/views/PublicComicList.vue
@@ -2,6 +2,7 @@
   <div>
       <public-collection v-bind:collectionId="parseInt(this.$route.params.collectionId)"/>
       <div class="public-collection">
+        <stats v-bind:collectionId="parseInt(this.$route.params.collectionId)"/>
       <router-link :to="{ name:'collections' }"> Back to Collection </router-link>
       </div>
   </div>
@@ -9,8 +10,10 @@
 
 <script>
 import PublicCollection from '../components/PublicCollection.vue'
+import Stats from '../components/Stats.vue'
+
 export default {
-  components: { PublicCollection },
+  components: { PublicCollection, Stats },
     name:'public-comic-list',
 
 
